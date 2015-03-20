b7ca91ed6599376894a414d2e3aaf8a86b6735b1
Tamas Cservenak <tamas@cservenak.net>
2015-03-20 16:18:58 +0100

    NEXUS-8178: Maven CMA group

---
+++ plugins/basic/nexus-repository-maven/src/main/java/org/sonatype/nexus/repository/maven/internal/MavenGroupFacet.java
@@ -0,0 +1,103 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-2015 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.maven.internal;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.sonatype.nexus.common.hash.HashAlgorithm;
+import org.sonatype.nexus.repository.Facet;
+import org.sonatype.nexus.repository.Repository;
+import org.sonatype.nexus.repository.group.GroupFacetImpl;
+import org.sonatype.nexus.repository.http.HttpStatus;
+import org.sonatype.nexus.repository.manager.RepositoryManager;
+import org.sonatype.nexus.repository.maven.internal.MavenPath.HashType;
+import org.sonatype.nexus.repository.view.Payload;
+import org.sonatype.nexus.repository.view.PayloadResponse;
+import org.sonatype.nexus.repository.view.Response;
+import org.sonatype.nexus.repository.view.payloads.BlobPayload;
+import org.sonatype.nexus.repository.view.payloads.StringPayload;
+
+import com.google.common.hash.HashCode;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * Maven specific implementation of {@link GroupFacetImpl}.
+ *
+ * @since 3.0
+ */
+@Named
+@Facet.Exposed
+public class MavenGroupFacet
+    extends GroupFacetImpl
+{
+  private MavenFacet mavenFacet;
+
+  @Inject
+  public MavenGroupFacet(final RepositoryManager repositoryManager) {
+    super(repositoryManager);
+  }
+
+  @Override
+  protected void doConfigure() throws Exception {
+    super.doConfigure();
+    this.mavenFacet = getRepository().facet(MavenFacet.class);
+  }
+
+  public BlobPayload getCachedMergedMetadata(final MavenPath mavenPath) throws IOException {
+    return mavenFacet.get(mavenPath);
+  }
+
+  public BlobPayload mergeAndCacheMetadata(final MavenPath mavenPath,
+                                           final LinkedHashMap<Repository, Response> responses) throws IOException
+  {
+    checkArgument(!mavenPath.isHash(), "Only metadata can be merged and cached!");
+    Payload mergedMetadata = null;
+    for (Map.Entry<Repository, Response> entry : responses.entrySet()) {
+      if (entry.getValue().getStatus().getCode() == HttpStatus.OK) {
+        final Response response = entry.getValue();
+        if (response instanceof PayloadResponse) {
+          final PayloadResponse payloadResponse = (PayloadResponse) response;
+
+          // TODO: do the merge, this is just fluke to work in testing
+          if (mergedMetadata == null) {
+            mergedMetadata = payloadResponse.getPayload();
+          }
+        }
+      }
+    }
+    if (mergedMetadata == null) {
+      return null;
+    }
+
+    cacheMergedMetadata(mavenPath, mergedMetadata);
+
+    return getCachedMergedMetadata(mavenPath);
+  }
+
+  public void cacheMergedMetadata(final MavenPath mavenPath, final Payload payload) throws IOException {
+    checkArgument(!mavenPath.isHash(), "only metadata can be cached!");
+    mavenFacet.put(mavenPath, payload);
+    final BlobPayload blobPayload = mavenFacet.get(mavenPath);
+    final HashCode sha1HashCode = blobPayload.getHashCodes().get(HashAlgorithm.SHA1);
+    mavenFacet.put(mavenPath.hash(HashType.SHA1), new StringPayload(sha1HashCode.toString(), "text/plain"));
+  }
+
+
+  // TODO: asset event listening and using MavenFacet.delete maybe to delete invalidated elementsl
+}
---
+++ plugins/basic/nexus-repository-maven/src/main/java/org/sonatype/nexus/repository/maven/internal/maven2/Maven2GroupRecipe.groovy
@@ -0,0 +1,124 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2008-2015 Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.nexus.repository.maven.internal.maven2
+
+import javax.annotation.Nonnull
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Provider
+
+import org.sonatype.nexus.repository.Format
+import org.sonatype.nexus.repository.RecipeSupport
+import org.sonatype.nexus.repository.Repository
+import org.sonatype.nexus.repository.Type
+import org.sonatype.nexus.repository.group.GroupFacetImpl
+import org.sonatype.nexus.repository.group.GroupHandler
+import org.sonatype.nexus.repository.maven.internal.GroupMetadataHandler
+import org.sonatype.nexus.repository.maven.internal.MavenArtifactMatcher
+import org.sonatype.nexus.repository.maven.internal.MavenFacetImpl
+import org.sonatype.nexus.repository.maven.internal.MavenGroupFacet
+import org.sonatype.nexus.repository.maven.internal.MavenMetadataMatcher
+import org.sonatype.nexus.repository.security.SecurityHandler
+import org.sonatype.nexus.repository.storage.StorageFacetImpl
+import org.sonatype.nexus.repository.types.GroupType
+import org.sonatype.nexus.repository.view.ConfigurableViewFacet
+import org.sonatype.nexus.repository.view.Route
+import org.sonatype.nexus.repository.view.Router
+import org.sonatype.nexus.repository.view.ViewFacet
+import org.sonatype.nexus.repository.view.handlers.TimingHandler
+
+import static org.sonatype.nexus.repository.http.HttpHandlers.notFound
+
+/**
+ * Raw group repository recipe.
+ *
+ * @since 3.0
+ */
+@Named(Maven2GroupRecipe.NAME)
+class Maven2GroupRecipe
+    extends RecipeSupport
+{
+  static final String NAME = "maven2-group"
+
+  @Inject
+  Provider<Maven2SecurityFacet> securityFacet
+
+  @Inject
+  Provider<ConfigurableViewFacet> viewFacet
+
+  @Inject
+  Provider<StorageFacetImpl> storageFacet
+
+  @Inject
+  Provider<MavenGroupFacet> mavenGroupFacet
+
+  @Inject
+  TimingHandler timingHandler
+
+  @Inject
+  SecurityHandler securityHandler
+
+  @Inject
+  Provider<MavenFacetImpl> mavenFacet
+
+  @Inject
+  GroupHandler groupHandler
+
+  @Inject
+  GroupMetadataHandler groupMetadataHandler
+
+  @Inject
+  Maven2GroupRecipe(@Named(GroupType.NAME) Type type,
+                    @Named(Maven2Format.NAME) Format format)
+  {
+    super(type, format)
+  }
+
+  @Override
+  void apply(@Nonnull final Repository repository) throws Exception {
+    repository.attach(securityFacet.get())
+    repository.attach(storageFacet.get())
+    repository.attach(mavenFacet.get())
+    repository.attach(mavenGroupFacet.get())
+    repository.attach(configure(viewFacet.get()))
+  }
+
+  /**
+   * Configure {@link ViewFacet}.
+   */
+  private ViewFacet configure(final ConfigurableViewFacet viewFacet) {
+    Router.Builder builder = new Router.Builder()
+
+    Maven2MavenPathParser pathParser = new Maven2MavenPathParser();
+
+    builder.route(new Route.Builder()
+        .matcher(new MavenArtifactMatcher(pathParser))
+        .handler(timingHandler)
+        .handler(securityHandler)
+        .handler(groupHandler)
+        .create())
+
+    builder.route(new Route.Builder()
+        .matcher(new MavenMetadataMatcher(pathParser))
+        .handler(timingHandler)
+        .handler(securityHandler)
+        .handler(groupMetadataHandler)
+        .create())
+
+    builder.defaultHandlers(notFound())
+
+    viewFacet.configure(builder.create())
+
+    return viewFacet
+  }
+}
